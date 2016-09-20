4db5c5d435213cce38c407cbaacb414e7eb51151
Jeannette DuVal <duval@g.harvard.edu>
2016-09-20 09:14:55 -0400

    first commit v2

---
+++ index.php
@@ -0,0 +1,24 @@
+<pre>
+TO INCLUDE:
+
+ Forms
+  Inputs
+    Checkboxes, radiobuttions
+
+    => SUBMIT to generator.
+      Taking form data,
+      Generating a password
+
+      array_shuffle or shuffle
+      rand()
+      // million ways to accomplish this
+      // i.e. taking API?
+      // scraping external site with English words
+
+      // scrape it only once and put it into text file (file processing in php)
+      // or to .cvs (there are php functions for that, and will put it into array)
+
+      // (trump pwd generator :) )
+
+      // chr - returns ascii character based ascii code
+</pre>
