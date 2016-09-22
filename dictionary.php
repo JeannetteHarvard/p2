5b7752675eaa72d8dc40e23400cfbf28f0744b41
Jeannette DuVal <duval@g.harvard.edu>
2016-09-21 23:13:37 -0400

    dictionary test

---
+++ dictionary.php
@@ -0,0 +1,17 @@
+<?php
+
+$contents = file_get_contents("https://en.wikipedia.org/wiki/Donald_Trump");
+
+$contents = strip_tags($contents);
+
+$contents = preg_split("#[\b\s]#", $contents);
+
+echo $contents;
+
+ ?>
+
+ <pre>
+
+<?php var_export($contents); ?>
+
+ </pre>
