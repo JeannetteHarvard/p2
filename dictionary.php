71a0e14eb30c1fdf2b7d0d8702fe10077a362d9c
Jeannette DuVal <duval@g.harvard.edu>
2016-09-21 23:17:14 -0400

    dictionary test

--- dictionary.php
+++ dictionary.php
@@ -4,7 +4,8 @@ $contents = file_get_contents("https://en.wikipedia.org/wiki/Donald_Trump");
 
 $contents = strip_tags($contents);
 
-$contents = preg_split("#[\b\s]#", $contents);
+//$contents = preg_split("#[\b\s]#", $contents);
+$contents = preg_split("#[\W\s]#", $contents);
 
 echo $contents;
 
