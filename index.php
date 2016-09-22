c2b34c35245928ad3f5c4631298e698fdf8a0b29
Jeannette DuVal <duval@g.harvard.edu>
2016-09-22 01:56:33 -0400

    minor visual changes

--- index.php
+++ index.php
@@ -17,15 +17,20 @@ require('dictionary.php')
 
 
   <style>
-    .password {
-      color: blue;
-      font-weight: bold;
-    }
   body {
       background-image: url(img/trump.jpg);
       background-size: cover;
       background-position: top center;
     }
+    .password {
+      font-size: 2em;
+      text-align: center;
+      color: #E39000;
+      font-weight: bold;
+      text-shadow: 2px 2px #7A4D00;
+    }
+
+
   </style>
 
 </head>
@@ -34,8 +39,6 @@ require('dictionary.php')
 
   <div class='container'>
     <h1>Trump Password Generator</h1>
-
-    <p class="password"><?php echo $password ?></p>
 
     <form>
       <label for='numberOfWords'>Number of Words</label>
@@ -54,7 +57,15 @@ require('dictionary.php')
       <input type='checkbox' name='includeASymbol' id='includeASymbol' <?php echo $includeASymbol ? 'checked' : '' ?>>
       <br>
 
-      <input type='submit' value='Generate New Password!'>
+      <input type='submit' value='Generate New Trump Password!'>
+      <br>
+      Note: the words are taken from https://en.wikipedia.org/wiki/Donald_Trump 
+
+      <br><br><br><br><br><br><br><br><br>
+
+      <p class="password"><?php echo $password ?></p>
+
+
 
 </body>
 </html>
