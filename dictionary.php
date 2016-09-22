2593d39671e0a9439af4e530544315e5ab8891ff
Jeannette DuVal <duval@g.harvard.edu>
2016-09-22 10:45:34 -0400

    minor update

--- dictionary.php
+++ dictionary.php
@@ -3,7 +3,8 @@
 // First, let's check whether the file words.csv exists, and if not, we'll parse Wikipedia Trump page for words
 if (!file_exists('words.csv')) {
 
-  $url = "https://en.wikipedia.org/wiki/Donald_Trump";
+  $url = "http://en.wikipedia.org/wiki/Donald_Trump";
+  echo $url;
   //$url = "http://www.politico.com/story/2016/07/full-transcript-donald-trump-nomination-acceptance-speech-at-rnc-225974";
   $contents = file_get_contents($url);
   $contents = strip_tags($contents);
