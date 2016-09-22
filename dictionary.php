9b891899eb39c67878d6bed838dd457102cb9207
Jeannette DuVal <duval@g.harvard.edu>
2016-09-21 23:58:51 -0400

    dictionary update

--- dictionary.php
+++ dictionary.php
@@ -6,18 +6,34 @@ $contents = strip_tags($contents);
 
 //$contents = preg_split("#[\b\s]#", $contents);
 $contents = preg_split("#[\W\s]#", $contents);
+// splits the string into array separating by non alphabetic characters
 
 //$contents_length = count($contents);
 //$contents = array_slice($contents, 2000, $contents_length-5000);
 $contents = array_slice($contents, 2000, -5000);
 
+function clean_my_words($words) {
+  return strtolower($words);
+}
 
-echo $contents;
+$clean_contents = array_map("clean_my_words", $contents);
+
+//$clean_contents = array_filter($clean_contents, function($e) { return strlen($e) > 3; });
+$clean_contents = array_filter($clean_contents, function ($e) { return strlen($e) > 3 && strlen($e) < 16; });
+
+$clean_contents = array_filter($clean_contents, function($e) { return !preg_match('#[^a-z]#', $e); });
+// preg is looking for any non alphabetical symbols, in that case retruns truth inverted to false by '!', in which case the array item is removed
+
+$clean_contents = array_unique($clean_contents);
+
+file_put_contents('words.txt', join($clean_contents, "\n"));
+
+echo $clean_contents;
 
  ?>
 
  <pre>
 
-<?php var_export($contents); ?>
+<?php var_export($clean_contents); ?>
 
  </pre>
