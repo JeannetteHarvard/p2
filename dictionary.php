a08896d59d3dfbf3c63111e7631b5f5dc298e07f
Jeannette DuVal <duval@g.harvard.edu>
2016-09-21 23:31:10 -0400

    dictionary update

--- dictionary.php
+++ dictionary.php
@@ -7,8 +7,9 @@ $contents = strip_tags($contents);
 //$contents = preg_split("#[\b\s]#", $contents);
 $contents = preg_split("#[\W\s]#", $contents);
 
-$contents_length = count($contents);
-$contents = array_slice($contents, 2000, $contents_length-5000);
+//$contents_length = count($contents);
+//$contents = array_slice($contents, 2000, $contents_length-5000);
+$contents = array_slice($contents, 2000, -5000);
 
 
 echo $contents;
